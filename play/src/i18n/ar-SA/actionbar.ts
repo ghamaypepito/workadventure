1855ade163b381ee297b175ee788fb7d07f1304a
Ghamay Pepito <ghameey@Ghamays-MacBook-Pro.local>
2026-07-20 22:46:38 +0800

    Reapply "Add Meeting View / Map View toggle"
    This reverts commit aeb6e1269f0e74199dbe6e3276f796c9f2da6cae.

--- play/src/i18n/ar-SA/actionbar.ts
+++ play/src/i18n/ar-SA/actionbar.ts
@@ -3,6 +3,8 @@ import type { DeepPartial } from "../DeepPartial";
 
 const actionbar: DeepPartial<Translation["actionbar"]> = {
     understand: "فهمت!",
+    mapView: "Map view",
+    meetingView: "Meeting view",
     edit: "تحرير",
     cancel: "إلغاء",
     close: "إغلاق",
--- play/src/i18n/de-DE/actionbar.ts
+++ play/src/i18n/de-DE/actionbar.ts
@@ -3,6 +3,8 @@ import type { DeepPartial } from "../DeepPartial";
 
 const actionbar: DeepPartial<Translation["actionbar"]> = {
     understand: "Verstanden!",
+    mapView: "Map view",
+    meetingView: "Meeting view",
     edit: "Bearbeiten",
     cancel: "Abbrechen",
     close: "Schließen",
